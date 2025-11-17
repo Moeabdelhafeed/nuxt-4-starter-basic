9c7ceec58e83ec44e9362b996e5771ab42098649
moeabdelhafeed <almormohammad939@gmail.com>
2025-11-17 11:02:25 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,54 @@
+import tailwindcss from "@tailwindcss/vite"
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  css:[
+    "~/assets/css/main.css",
+  ],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ]
+  },
+
+  i18n: {
+    baseUrl: 'https://aqarygo.com',
+    locales: [
+      { code: 'en', language: 'en-US', file: 'en.json', name: 'English', dir: 'ltr' },
+      { code: 'ar', language: 'ar-SA', file: 'ar.json', name: 'العربية', dir: 'rtl' }
+    ],
+    strategy: 'no_prefix',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root', // recommended
+      fallbackLocale: 'en',
+    },
+  },
+
+    shadcn: {
+      /**
+       * Prefix for all the imported component.
+       * @default "Ui"
+       */
+      prefix: '',
+      /**
+       * Directory that the component lives in.
+       * Will respect the Nuxt aliases.
+       * @link https://nuxt.com/docs/api/nuxt-config#alias
+       * @default "@/components/ui"
+       */
+      componentDir: '@/components/ui'
+    },
+
+  
+
+  
+
+  modules: ["@nuxtjs/i18n", 'shadcn-nuxt', '@vueuse/nuxt'],
+})
